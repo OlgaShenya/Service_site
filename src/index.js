bfa6aa22fc1c12cd7a41df1238a3b4345d0854a3
OlgaSh <Olga.Shenya@gmail.com>
2022-11-12 13:43:25 +0300

    html main page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 import './index.html';
 import './index.scss';
-import code from './img/code.png';
-
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append(img);
-
-
-const mult = (a, b) => a * b;
-
-console.log(mult(2, 4));
